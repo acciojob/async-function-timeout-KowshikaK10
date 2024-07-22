20f0d19db5b6150e420eb27bc5d7d9b26b7dd63f
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-22 18:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const textInput=document.getElementById('text');
+const delayInput=document.getElementById('delay');
+const btn=document.getElementById('btn');
+const output=document.getElementById('output');
+
+btn.addEventListener('click',async(e)=>{
+	e.preventDefault();
+	await delayFun(parseInt(delayInput.value));
+    output.textContent = textInput.value;
+	
+	
+	async function delayFun(delay) {
+        return new Promise(resolve => setTimeout(resolve, delay));
+   }
+});
